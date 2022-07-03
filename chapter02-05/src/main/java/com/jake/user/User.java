0d938f008e330cc05663b5346457bd517ac8d472
1800479 <>
2022-07-03 15:30:16 +0800

    springboot調整sprinfcloud

--- chapter02-05/src/main/java/com/jake/user/User.java
+++ chapter02-05/src/main/java/com/jake/user/User.java
@@ -2,7 +2,9 @@ package com.jake.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class User {
     public static void main(String[] args) {
