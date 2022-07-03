e6817c6435d790c137beaed9424b3464b931b348
1800479 <>
2022-07-03 10:58:46 +0800

    調整

--- chapter02-user/src/main/java/com/jake/user/Dao/UserSmsCodeDao.java
+++ chapter02-user/src/main/java/com/jake/user/Dao/UserSmsCodeDao.java
@@ -3,6 +3,7 @@ package com.jake.user.Dao;
 import com.jake.user.entity.UserSmsCode;
 import org.apache.ibatis.annotations.Mapper;
 
+
 @Mapper
 public interface UserSmsCodeDao {
     int insert(UserSmsCode userSmsCode);
